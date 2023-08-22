8db9c043b34335fdc0689087c4f81a8c63ea3fee
Attraya Das <attrayaghoshdas@gmail.com>
2023-08-22 22:20:20 +0530

    Create four Microservices - OrderService, StockService, EmailService & Base-Domains

---
+++ base-domains/src/test/java/com/attraya/basedomains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attraya.basedomains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
